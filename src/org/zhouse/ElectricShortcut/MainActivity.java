fd740ffc6b0147b3fab8b8d8852855b1ad8b6a19
szymon-marsula <sz.marsula@gmail.com>
2015-01-27 16:56:19 +0100

    aaa

--- src/org/zhouse/ElectricShortcut/MainActivity.java
+++ src/org/zhouse/ElectricShortcut/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     	
-    	//glowna funkcja
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
@@ -52,6 +51,7 @@ public class MainActivity extends Activity {
         new JSONAsyncTask().execute("");
         
     }
+    
     
   //**********
   //ASYNC TASK
